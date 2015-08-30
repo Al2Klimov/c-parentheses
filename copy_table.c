8a0b9ffe91bc38ac4582fc4c51a703baa3b4e2f6
Alexander A. Klimov <Al2Klimov@users.noreply.github.com>
2015-08-30 12:14:49 +0200

    Implement copy tables

---
+++ copy_table.c
@@ -0,0 +1,115 @@
+/* C() / "C Parentheses"
+ * A pure functional programming language
+ * Reference implementation
+ *
+ * Copyright (C) 2015  Alexander A. Klimov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+// Function definitions for copy tables
+
+
+#include <stddef.h>
+// NULL
+// size_t
+
+#include <stdbool.h>
+// bool
+
+#include <stdlib.h>
+// malloc()
+// realloc()
+// free()
+
+#include "copy_table.h"
+// cprnths_copytab_t
+// cprnths_copytab_row_t
+
+#include "reference.h"
+// cprnths_ref_t
+
+
+cprnths_copytab_t* cprnths_copytab_create(size_t const s) {
+    cprnths_copytab_t *restrict t = malloc(sizeof(cprnths_copytab_t));
+
+    if (t == NULL)
+        goto Finish;
+
+    t->tab = malloc(s * sizeof(cprnths_copytab_row_t));
+
+    if (t->tab == NULL) {
+        free(t);
+        t = NULL;
+        goto Finish;
+    }
+
+    {
+        cprnths_copytab_row_t *restrict p = t->tab + s;
+        do {
+            (--p)->orig = NULL;
+            p->copy = NULL;
+        } while (p > t->tab);
+    }
+
+    *(size_t*)&t->chunksize = t->slots_total = t->slots_free = s;
+
+Finish:
+    return t;
+}
+
+bool cprnths_copytab_addrefs(cprnths_copytab_t *restrict const t, cprnths_ref_t *const o, cprnths_ref_t *const c) {
+    if (t->slots_free) {
+        cprnths_copytab_row_t *restrict T = t->tab;
+        while (T->orig != NULL)
+            ++T;
+        T->orig = o;
+        T->copy = c;
+        --t->slots_free;
+    } else {
+        {
+            size_t const slots_total = t->slots_total + t->chunksize;
+
+            {
+                cprnths_copytab_row_t *restrict T = realloc(
+                    t->tab,
+                    slots_total * sizeof(cprnths_copytab_row_t)
+                );
+                if (T == NULL)
+                    return false;
+
+                t->tab = T;
+                T += t->slots_total;
+                T->orig = o;
+                T->copy = c;
+
+                cprnths_copytab_row_t *const limit = t->tab + slots_total;
+                while (++T < limit)
+                    T->orig = T->copy = NULL;
+            }
+
+            t->slots_total = slots_total;
+        }
+
+        t->slots_free += t->chunksize - 1u;
+    }
+
+    return true;
+}
+
+void cprnths_copytab_destruct(cprnths_copytab_t *restrict const t) {
+    free(t->tab);
+    free(t);
+}
---
+++ copy_table.h
@@ -0,0 +1,83 @@
+/* C() / "C Parentheses"
+ * A pure functional programming language
+ * Reference implementation
+ *
+ * Copyright (C) 2015  Alexander A. Klimov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+// Typedefs and functions for copy tables
+#ifndef CPARENTHESES_INCLUDE_COPY_TABLE
+#define CPARENTHESES_INCLUDE_COPY_TABLE
+
+
+struct cpintern_copytab_t;
+typedef struct cpintern_copytab_t cprnths_copytab_t;
+
+
+#include <stddef.h>
+// size_t
+
+#include <stdbool.h>
+// bool
+
+#include "reference.h"
+// cprnths_ref_t
+
+
+// A row of a copy table (just for easy reusing)
+typedef struct {
+    // The original
+    cprnths_ref_t* orig;
+
+    // The copy
+    cprnths_ref_t* copy;
+} cprnths_copytab_row_t;
+
+// This is for tracking which references have already been copied during deep copying
+// not to copy a reference more than once. (You know, diamond references, ...)
+struct cpintern_copytab_t {
+    // The table itself
+    cprnths_copytab_row_t* tab;
+
+    // Amount of slots to allocate memory for at once
+    // 1 slot == sizeof( cprnths_copytab_row_t )
+    // SHALL be > 0!
+    size_t const chunksize;
+
+    // Amount of present slots
+    // SHALL be > 0 and multiple of .chunksize!
+    size_t slots_total;
+
+    // Amount of free slots
+    // SHALL be <= .slots_total!
+    size_t slots_free;
+};
+
+
+// Create a new copy table with given .chunksize and return a pointer to it.
+// Return NULL if something went wrong.
+cprnths_copytab_t* cprnths_copytab_create(size_t);
+
+// Add the given references (original and copy) to the given copy table.
+// Return true if (and only if) the references were added successfully.
+bool cprnths_copytab_addrefs(cprnths_copytab_t*, cprnths_ref_t*, cprnths_ref_t*);
+
+// Destruct and free() the given copy table.
+void cprnths_copytab_destruct(cprnths_copytab_t*);
+
+
+#endif
