1ff9fab76453616a6d1382d345ba3ba576ae8519
Alexander A. Klimov <Al2Klimov@users.noreply.github.com>
2015-12-04 20:17:08 +0100

    Drop cprnths_obj_cls_lval()

--- object.h
+++ object.h
@@ -42,13 +42,6 @@ struct cprnths_obj_t {
 };
 
 
-// This is for setting the ->cls initially on any type of object.
-// Usage: cprnths_obj_cls_lval(x) = c
-// x MUST be a pointer to the target object.
-// This works only if the target's storage was allocated with malloc() or realloc().
-#define cprnths_obj_cls_lval(x) (((cprnths_obj_t*)(x))->cls)
-
-
 // About the following functions:
 //
 // The pointer arguments MUST NOT be NULL.
