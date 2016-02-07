fb4fe00751631a6e44e9b412b65992c10e99f778
Alexander A. Klimov <Al2Klimov@users.noreply.github.com>
2016-02-07 14:17:01 +0100

    Implement cprnths_expr_eval()

--- exec.c
+++ exec.c
@@ -39,6 +39,7 @@
 #include "expr.h"
 // cprnths_expr_t
 // cprnths_exprs_t
+// cprnths_expr_eval()
 
 #include "garbage_table.h"
 // cprnths_garbtab_create()
@@ -116,9 +117,7 @@ cprnths_exec(
     cprnths_error_t err;
     {
         struct cprnths_expr_t const *const *restrict current = (struct cprnths_expr_t const *const *)exprs->exprs;
-        while (!(
-            err = (*(*current)->cls->expr_eval)(*current, env, NULL)
-        ) && *++current != NULL);
+        while (!( err = cprnths_expr_eval(*current, env, NULL) ) && *++current != NULL);
     }
     return err;
 }
